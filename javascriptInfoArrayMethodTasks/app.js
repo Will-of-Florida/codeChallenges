f6ee0c6465e0f9ee42ab3c5b4a908c0da8e4ed33
Will-Of-Florida <will_of_florida@live.com>
2022-08-10 12:25:45 -0400

    Daily Coding Challenge.

--- javascriptInfoArrayMethodTasks/app.js
+++ javascriptInfoArrayMethodTasks/app.js
@@ -223,3 +223,199 @@ alert( usersMapped[0].id ) // 1
 alert( usersMapped[0].fullName ) // John Smith
 
 
+
+// ********************
+// ** QUESTION SEVEN **
+// ********************
+// Sort users by age
+// importance: 5
+// Write the function sortByAge(users) that gets an array of objects with the age property and sorts them by age.
+
+// For instance:
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+// sortByAge(arr);
+
+// // now: [john, mary, pete]
+// alert(arr[0].name); // John
+// alert(arr[1].name); // Mary
+// alert(arr[2].name); // Pete
+
+function sortByAge(arr){
+    arr.sort(
+        function(a, b){
+            return a.age - b.age
+        }
+    )
+};
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+sortByAge(arr);
+
+// now: [john, mary, pete]
+alert(arr[0].name); // John
+alert(arr[1].name); // Mary
+alert(arr[2].name); // Pete
+
+
+
+// ********************
+// ** QUESTION EIGHT **
+// ********************
+
+// Shuffle an array
+// importance: 3
+// Write the function shuffle(array) that shuffles (randomly reorders) elements of the array.
+
+// Multiple runs of shuffle may lead to different orders of elements. For instance:
+
+// let arr = [1, 2, 3];
+
+// shuffle(arr);
+// // arr = [3, 2, 1]
+
+// shuffle(arr);
+// // arr = [2, 1, 3]
+
+// shuffle(arr);
+// // arr = [3, 1, 2]
+// // ...
+// All element orders should have an equal probability. For instance, [1,2,3] can be reordered as [1,2,3] 
+// or [1,3,2] or [3,1,2] etc, with equal probability of each case.
+
+
+function shuffle(array){
+    array.sort(( => Math.random()- 0.5))
+}
+let array = [1, 2, 3]
+shuffle(array)
+console.log(array)
+
+
+
+
+// ********************
+// ** QUESTION NINE **
+// ********************
+// Get average age
+// importance: 4
+// Write the function getAverageAge(users) that gets an array of objects with property age and returns the average age.
+
+// The formula for the average is (age1 + age2 + ... + ageN) / N.
+
+// For instance:
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+
+// let arr = [ john, pete, mary ];
+
+// alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+function getAverageAge(users){
+    return users.reduce((a, b) => a.age + b, 0) / users.length;
+}
+  
+  let john = { name: "John", age: 25 };
+  let pete = { name: "Pete", age: 30 };
+  let mary = { name: "Mary", age: 29 };
+  
+  let arr = [ john, pete, mary ];
+  
+  alert( getAverageAge(arr) ); // 28
+
+  
+
+// ********************
+// *** QUESTION TEN ***
+// ********************
+// Filter unique array members
+// importance: 4
+// Let arr be an array.
+
+// Create a function unique(arr) that should return an array with unique items of arr.
+
+// For instance:
+
+// function unique(arr) {
+//   /* your code */
+// }
+
+// let strings = ["Hare", "Krishna", "Hare", "Krishna",
+//   "Krishna", "Krishna", "Hare", "Hare", ":-O"
+// ];
+
+// alert( unique(strings) ); // Hare, Krishna, :-O
+
+function unique(arr) {
+    let result = [];
+  
+    for (let str of arr) {
+      if (!result.includes(str)) {
+        result.push(str);
+      }
+    }
+  
+    return result;
+  }
+  
+  let strings = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+  ];
+  
+  alert( unique(strings) ); // Hare, Krishna, :-O
+
+
+
+
+// ********************
+// ** QUESTION ELEVEN *
+// ********************
+//   Create keyed object from array
+//   importance: 4
+//   Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+  
+//   Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+  
+//   For example:
+  
+//   let users = [
+//     {id: 'john', name: "John Smith", age: 20},
+//     {id: 'ann', name: "Ann Smith", age: 24},
+//     {id: 'pete', name: "Pete Peterson", age: 31},
+//   ];
+  
+//   let usersById = groupById(users);
+  
+//   /*
+//   // after the call we should have:
+  
+//   usersById = {
+//     john: {id: 'john', name: "John Smith", age: 20},
+//     ann: {id: 'ann', name: "Ann Smith", age: 24},
+//     pete: {id: 'pete', name: "Pete Peterson", age: 31},
+//   }
+//   */
+//   Such function is really handy when working with server data.
+  
+//   In this task we assume that id is unique. There may be no two array items with the same id.
+  
+//   Please use array .reduce method in the solution.
+
+function groupById(array) {
+    return array.reduce((obj, value) => {
+      obj[value.id] = value;
+      return obj;
+    }, {})
+  }
