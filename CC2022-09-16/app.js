11659483ae317cee1cd6d5080253460efcb3a86a
Will-Of-Florida <will_of_florida@live.com>
2022-09-16 20:46:00 -0400

    Daily Coding Challenge

---
+++ CC2022-09-16/app.js
@@ -0,0 +1,20 @@
+// The goal of this kata is to write a function that takes two inputs: 
+// a string and a character. The function will count the number of times 
+// that character appears in the string. The count is case insensitive.
+
+// For example:
+
+// countChar("fizzbuzz","z") => 4
+
+// countChar("Fancy fifth fly aloof","f") => 5
+
+// The character can be any alphanumeric character.
+
+// Good luck.
+
+function countChar(string, char) {
+    let caseInsensitve = string.toLowerCase()
+    let characterLowerCase = char.toLowerCase()
+    let array = caseInsensitve.split(characterLowerCase)
+    return array.length - 1
+    }
